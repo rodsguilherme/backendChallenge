73a631716aebc339936af0cf5f48bccf7c8f175f
GuilhermeRods <rodsguilherme@gmail.com>
2019-12-22 18:17:17 -0300

    altera estrutura da criacao de vagas

--- api/controllers/vacancyController.js
+++ api/controllers/vacancyController.js
@@ -1,30 +1,25 @@
-import koa from 'koa';
-import Router from 'koa-router';
-const router = new Router({ prefix: '/v1' });
-const api = new koa()
 
 import { createVacancy } from '../services/vacancyService'
 
-router.post('/vacancys', async ctx => {
-    const { body } = ctx.request
-    const vacancy = {
-        name: body.name,
-        skills: body.skills,
-        description: body.description
-    }
-    try {
-        await createVacancy(vacancy)
-        ctx.body = {
-            message: 'Vaga criada com sucesso!'
+const VacancyController = {
+    create: async ctx => {
+        const { body } = ctx.request
+        const vacancy = {
+            name: body.name,
+            skills: body.skills,
+            description: body.description
         }
-        ctx.status = 201
-    } catch (error) {
-        ctx.body = error
-        ctx.status = 400
+        try {
+            await createVacancy(vacancy)
+            ctx.body = {
+                message: 'Vaga criada com sucesso!'
+            }
+            ctx.status = 201
+        } catch (error) {
+            ctx.body = { message: error }
+            ctx.status = 400
+        }
     }
-})
+}
 
-
-
-api.use(router.routes())
-module.exports = api
+export default VacancyController
