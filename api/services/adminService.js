1c5ce5c72ccec869f0c7c19424409e54f369d18d
GuilhermeRods <rodsguilherme@gmail.com>
2019-12-22 17:30:56 -0300

    altera verificacao de admin

--- api/services/adminService.js
+++ api/services/adminService.js
@@ -1,24 +1,16 @@
 import database from '../database/connect'
 import { emailValidation } from './validationService'
-import {  createHash } from './cryptografyService';
+import { createHash } from './cryptografyService';
 
 const createAdmin = async admin => {
     const { name, password, email } = admin
 
-   
-
-    if (!emailIsValid(email)) {
+    if (!emailValidation(email)) {
         throw 'Verifique o email e tente novamente'
     }
     const passwordHashed = createHash(password)
     await database('Admin').insert({ name, password: passwordHashed, email })
 
 }
-
-const emailIsValid = email => {
-    
-    return emailValidation(email)
-}
-
 
 module.exports = { createAdmin }
