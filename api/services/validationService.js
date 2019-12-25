10aff8b399025b5d2215ca360f919d4380bf2203
GuilhermeRods <rodsguilherme@gmail.com>
2019-12-25 03:14:42 -0300

    altera exportacao

--- api/services/validationService.js
+++ api/services/validationService.js
@@ -1,14 +1,12 @@
 
-const emailValidation = email => {
+export const emailValidation = email => {
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     return emailRegex.test(String(email).toLowerCase())
 
 };
 
-const telephoneValidation = telephone => {
+export const telephoneValidation = telephone => {
     const telephoneRegex = /\(\d{2,}\) \d{4,}\-\d{4}/
     return telephoneRegex.test(telephone)
 
 };
-
-module.exports = { emailValidation, telephoneValidation }
